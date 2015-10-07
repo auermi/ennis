eb5b11c7fa1a0f2843a63bd654c80fedf0e223c2
Michael Andrew Auer <michael@michael-auer.co>
2015-10-07 16:52:51 -0400

    Add footer with theme details

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<footer>
+  <h6><?php echo get_bloginfo('name') ?>-&nbsp;<?php echo get_bloginfo('description') ?></h6>
+  <h6>Ennis theme by <a href="http://twitter.com/auermi">Michael Auer</a></h6>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
