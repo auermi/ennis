704c2d72a736f7ffb1fc09ae145b2e481058d790
Michael Andrew Auer <michael@michael-auer.co>
2015-10-07 15:44:33 -0400

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,11 @@
+<? get_header(); ?>
+<div class="wrap">
+  <div class="left">
+      <h3>Error (404)</h3>
+      <p>This isn't the page you're looking for.</p>
+    <br>
+    <br>
+    <a class="back" href="<?php echo home_url() ?>">&#8592;&nbsp;Back to home</a>
+  </div>
+</div>
+<? get_footer(); ?>
