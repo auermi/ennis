03ae16c849f951d2f59611cbe652844f57f72da2
Michael Andrew Auer <michael@michael-auer.co>
2015-10-08 10:14:23 -0400

    Site title in footer links to site home

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <h6><?php echo get_bloginfo('name') ?>-&nbsp;<?php echo get_bloginfo('description') ?></h6>
+  <h6><a href="<?php echo home_url() ?>"><?php echo get_bloginfo('name') ?></a>-&nbsp;<?php echo get_bloginfo('description') ?></h6>
   <h6>Ennis theme by <a href="http://twitter.com/auermi">Michael Auer</a></h6>
 </footer>
 <?php wp_footer(); ?>
